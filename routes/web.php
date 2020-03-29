1d3ec4457ebeab93df0187483fbf13fb2bd3f15a
aditiakusuma <aditiakusuma800@gmail.com>
2020-03-29 08:38:57 +0700

    membuat auth dan menambah layout dan folder admilte

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('layouts.main');
+});
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
